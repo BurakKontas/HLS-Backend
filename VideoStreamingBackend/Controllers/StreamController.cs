02fafe2e58047b2302e9717e4dd4e6a60746880a
BurakKontas <aburakkontas@trakya.edu.tr>
2024-06-05 16:03:16 +0300

    # Not creating master.m3u8 file error fixed

--- VideoStreamingBackend/Controllers/StreamController.cs
+++ VideoStreamingBackend/Controllers/StreamController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Resources;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Xabe.FFmpeg;
@@ -13,7 +12,7 @@ namespace VideoStreamingBackend.Controllers
     public class StreamController : ControllerBase
     {
         private static readonly string VideoFilePath = @"C:\Users\konta\Desktop\test.mp4";
-        private static readonly string outputDirectory = @"C:\Users\konta\source\repos\VideoStreamingBackend\VideoStreamingBackend\output_directory\test";
+        private static readonly string outputDirectory = @"C:\Users\konta\source\repos\VideoStreamingBackend\VideoStreamingBackend\output_directory\";
 
 
         [HttpGet("{streamName}/{ts}")]
@@ -82,23 +81,24 @@ namespace VideoStreamingBackend.Controllers
 
             Directory.CreateDirectory(outputDirectory);
 
+            // https://ottverse.com/hls-packaging-using-ffmpeg-live-vod/
             var ffmpegArgsList = new List<string>
             {
                 $"-i \"{VideoFilePath}\"",
-                "-filter_complex \"[0]split=3[v1][v2][v3]; [v1]copy[v1out]; [v2]scale=w=1280:h=720[v2out]; [v3]scale=w=640:h=360[v3out]\"",
-                "-map \"[v1out]\"",
-                "-c:v:0 libx264 -x264-params \"nal-hrd=cbr:force_cfr=1\" -b:v:0 5M -maxrate:v:0 5M -minrate:v:0 5M -bufsize:v:0 10M -preset slow -g 48 -sc_threshold 0 -keyint_min 48",
-                "-map \"[v2out]\"",
-                "-c:v:1 libx264 -x264-params \"nal-hrd=cbr:force_cfr=1\" -b:v:1 3M -maxrate:v:1 3M -minrate:v:1 3M -bufsize:v:1 3M -preset slow -g 48 -sc_threshold 0 -keyint_min 48",
-                "-map \"[v3out]\"",
-                "-c:v:2 libx264 -x264-params \"nal-hrd=cbr:force_cfr=1\" -b:v:2 1M -maxrate:v:2 1M -minrate:v:2 1M -bufsize:v:2 1M -preset slow -g 48 -sc_threshold 0 -keyint_min 48",
+                "-filter_complex \"[0:v]split=3[v1][v2][v3]; [v1]copy[v1out]; [v2]scale=w=1280:h=720[v2out]; [v3]scale=w=640:h=360[v3out]\"",
+                "-map \"[v1out]\" -c:v:0 libx264 -x264-params \"nal-hrd=cbr:force-cfr=1\" -b:v:0 5M -maxrate:v:0 5M -minrate:v:0 5M -bufsize:v:0 10M -preset slow -g 48 -sc_threshold 0 -keyint_min 48",
+                "-map \"[v2out]\" -c:v:1 libx264 -x264-params \"nal-hrd=cbr:force-cfr=1\" -b:v:1 3M -maxrate:v:1 3M -minrate:v:1 3M -bufsize:v:1 3M -preset slow -g 48 -sc_threshold 0 -keyint_min 48",
+                "-map \"[v3out]\" -c:v:2 libx264 -x264-params \"nal-hrd=cbr:force-cfr=1\" -b:v:2 1M -maxrate:v:2 1M -minrate:v:2 1M -bufsize:v:2 1M -preset slow -g 48 -sc_threshold 0 -keyint_min 48",
                 "-map a:0 -c:a:0 aac -b:a:0 96k -ac 2",
                 "-map a:0 -c:a:1 aac -b:a:1 96k -ac 2",
                 "-map a:0 -c:a:2 aac -b:a:2 48k -ac 2",
-                "-f hls -hls_time 2",
-                "-hls_playlist_type vod -hls_flags independent_segments -hls_segment_type mpegts",
+                "-f hls",
+                "-hls_time 2",
+                "-hls_playlist_type vod",
+                "-hls_flags independent_segments",
+                "-hls_segment_type mpegts",
                 $"-hls_segment_filename \"{outputDirectory}\\stream_%v\\data%02d.ts\"",
-                $"-master_pl_name \"{outputDirectory}\\master.m3u8\"",
+                $"-master_pl_name master.m3u8",
                 "-var_stream_map \"v:0,a:0 v:1,a:1 v:2,a:2\"",
                 $"{outputDirectory}\\stream_%v.m3u8"
             };
